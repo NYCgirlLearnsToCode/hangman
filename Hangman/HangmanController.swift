ba482e51b0d223e2b77d0a7de06b013eb7b6b210
Lisa Jiang <ljiang@fuzzproductions.com>
2018-12-02 17:34:12 -0500

    Get random word

--- Hangman/HangmanController.swift
+++ Hangman/HangmanController.swift
@@ -10,18 +10,34 @@ import UIKit
 
 class HangmanController: UIViewController {
 	var words = ""
+	var wordsArr = [String.SubSequence]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .blue
+		view.backgroundColor = .white
 		getWords()
-		print(words)
 	}
 
 	func getWords() {
-		WordsClient.manager.getWords(success: { self.words = $0 })
-		DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute:  {
-			print(self.words.count)
+		WordsClient.manager.getWords(success: { [unowned self] in self.words = $0 })
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute:  { [unowned self] in
+			self.wordsIntoArray()
 		})
+	}
+	
+	func wordsIntoArray() {
+		wordsArr = words.split(separator: "\n")
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute:  { [unowned self] in
+			self.randomWord()
+		})
+	}
+	
+	func randomWord() {
+		let random = Int(arc4random_uniform(162414))
+		print(wordsArr.count)
+		print("random#", random)
+		let randomWord = wordsArr[random]
+		print("randomWord", randomWord)
 	}
 }
 
---
+++ Hangman/HangmanView.swift
@@ -0,0 +1,13 @@
+//
+//  HangmanView.swift
+//  Hangman
+//
+//  Created by Lisa Jiang on 12/2/18.
+//  Copyright © 2018 Lisa Jiang. All rights reserved.
+//
+
+import UIKit
+
+class HangmanView: UIView {
+
+}
